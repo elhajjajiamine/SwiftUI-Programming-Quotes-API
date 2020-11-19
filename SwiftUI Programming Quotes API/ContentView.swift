96527bf001b86b432d53423917b1fbebe3ab0c19
elhajjajiamine <aminselhajjaji@gmail.com>
2020-11-19 11:34:13 +0100

    Initial Commit

---
+++ SwiftUI Programming Quotes API/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Programming Quotes API
+//
+//  Created by elhajjaji on 19/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI Programming Quotes API/SwiftUI_Programming_Quotes_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Programming_Quotes_APIApp.swift
+//  SwiftUI Programming Quotes API
+//
+//  Created by elhajjaji on 19/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Programming_Quotes_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
