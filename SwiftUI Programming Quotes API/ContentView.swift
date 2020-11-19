b18dbb33f610b3f2d59d1e26da9760c65793948b
elhajjajiamine <aminselhajjaji@gmail.com>
2020-11-19 12:38:19 +0100

    obtain data from an API and parse the JSON response obtained.

--- SwiftUI Programming Quotes API/ContentView.swift
+++ SwiftUI Programming Quotes API/ContentView.swift
@@ -8,9 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var quoteData : QuotesData?
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+            VStack(){
+                Spacer()
+
+                Text(quoteData?.quote ?? "There are only two hard things in Computer Science: cache invalidation, naming things and off-by-one errors.")
+                    .font(.headline)
+                    .multilineTextAlignment(.leading)
+                    
+                HStack{
+                    Spacer()
+                    Text(quoteData?.author ?? "Phil Karlton")
+                        .font(.footnote)
+                        .fontWeight(.light)
+                        .padding(.leading)
+                }
+                
+                Spacer()
+                Button(action: loadData) {
+                    Image(systemName: "arrow.clockwise")
+                }
+                .font(.title)
+                .padding(.top)
+                    
+                
+                
+            }.padding()
+            .onAppear(perform: loadData)
+        
+    }
+    
+    
+    private func loadData() {
+        
+        guard let url = URL(string: "http://quotes.stormconsultancy.co.uk/random.json") else {
+            return
+        }
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let  data = data else {return}
+            if let decodedData = try? JSONDecoder().decode(QuotesData.self, from: data){
+                DispatchQueue.main.async {
+                    self.quoteData = decodedData
+                }
+            }
+        }.resume()
     }
 }
 
@@ -19,3 +62,15 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
+struct QuotesData : Decodable {
+    
+    var author : String
+    var quote : String
+    var id : Int
+    var permalink : String
+    
+}
+
